03ea916eba9a7f8d31fa73321f5dad0a5c8c795d
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2025-05-02 05:43:05 +0100

    Added hover,click,rotate,scale animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     show: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.25,
+        staggerChildren: 0.3,
       },
     },
   };
@@ -27,22 +27,58 @@ function App() {
             className="flex items-center justify-center rounded-lg aspect-square bg-slate-800 gap-10"
           >
             <motion.div
-            initial={{opacity:0,y:100}}
-            animate={{opacity:1,y:1}}
-            transition={{duration:1, ease:"easeOut",delay:0.2,}}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 1 }}
+              transition={{ duration: 1, ease: "easeIn", delay: 0.2 }}
               className="bg-stone-100 h-20 w-20 rounded-lg"
-           
             ></motion.div>
-            <motion.div className="bg-stone-100 h-20 w-20 rounded-full"></motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: -50 }}
+              animate={{ opacity: 1, y: 1 }}
+              transition={{ duration: 1, ease: "easeIn", delay: 0.2 }}
+              className="bg-stone-100 h-20 w-20 rounded-full"
+            ></motion.div>
           </motion.div>
+
           <motion.div
             variants={gridDivVariant}
             className="flex items-center justify-center rounded-lg aspect-square bg-slate-800 gap-10"
-          ></motion.div>
+          >
+            <motion.div
+              className="bg-rose-400 h-16 w-16"
+              animate={{
+                scale: [1, 2, 2, 1],
+                borderRadius: ["10%", "10%", "50%", "10%"],
+                rotate: [0, 90, 90, 0],
+              }}
+              transition={{
+                duration: 5,
+                ease: "easeInOut",
+                repeat: Infinity,
+                repeatDelay: 2,
+              }}
+            ></motion.div>
+          </motion.div>
+
           <motion.div
             variants={gridDivVariant}
             className="flex items-center justify-center rounded-lg aspect-square bg-slate-800 gap-10"
-          ></motion.div>
+          >
+            <motion.button
+              whileTap={{ scale: 0.9 }}
+              whileHover={{
+                scale: 1.1,
+                backgroundColor: "#d1d5db",
+                color: "black",
+              }}
+              transition={{
+                bounceDamping:10,bounceStiffness:600
+              }}
+              className="bg-emerald-400 w-1/2 py-4 rounded-lg text-white font-light tracking-wide"
+            >
+              Subscribe
+            </motion.button>
+          </motion.div>
           <motion.div
             variants={gridDivVariant}
             className="flex items-center justify-center rounded-lg aspect-square bg-slate-800 gap-10"
