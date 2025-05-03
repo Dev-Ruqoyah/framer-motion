659858297154f3ce665b9d0ef58a9f5a7cb7da9a
Dev-Ruqoyah <hamzatrukayatabiodun02@gmail.com>
2025-05-03 05:37:28 +0100

    framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,19 @@ function App() {
       },
     },
   };
-  const svgContainerVariant = {
+
+  const svgIconVariants = {
     hidden: {
       opacity: 0,
-      pathLength: 1,
-      fil: "rgba(252,211,77,0)",
+      pathLength: 0,
+      fill: "rgba(252, 211, 77, 0)",
     },
     visible: {
       opacity: 1,
       pathLength: 1,
-      fill: "rgba(252,211,77,0)",
+      fill: "rgba(252, 211, 77, 1)",
     },
-  };
+  }
   const { scrollYProgress: completionProgress } = useScroll();
 
   const gridDivVariant = { hidden: { opacity: 0 }, show: { opacity: 1 } };
@@ -141,15 +142,25 @@ function App() {
             >
               <motion.path
                 d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"
-                variants={svgContainerVariant}
+                variants={svgIconVariants}
                 initial="hidden"
                 animate="visible"
                 transition={{
+                  default:{
+                    duration:2,
+                    ease:"easeInOut",
+                    delay:1,
+                    repeat:Infinity,
+                    repeatType:"reverse",
+                    repeatDelay:1
+                  },
                   fill: {
                     duration: 2,
                     ease: "easeIn",
                     delay: 2,
                     repeat: Infinity,
+                    repeatType:"reverse",
+                    repeatDelay:1
                   },
                 }}
               />
